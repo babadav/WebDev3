973b845e2dc1cb09cc8a50adec8b2f8eed1d74fe
babadav <babadav@gmail.com>
2017-04-20 18:02:43 -0400

    1st

---
+++ Hero vs Villain/assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+	$('.bat-dart').click(function(){
+
+		$('.bat-hero-wrap').toggleClass('visible');
+		$('.bat-description-wrap').toggleClass('visible');
+
+		
+	});
+
+	$('.joker-logo').click(function(){
+		$('.joker-hero-wrap').toggleClass('visible');
+		$('.joker-alt-description-wrap').toggleClass('visible');
+	})
+
+});
+
