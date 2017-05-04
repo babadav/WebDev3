8a705254483a6cd4f766ea55aeeec4bb4cb98fb0
babadav <babadav@gmail.com>
2017-05-04 17:41:30 -0400

    05-04-17 b4 class

--- Hero vs Villain/assets/js/main.js
+++ Hero vs Villain/assets/js/main.js
@@ -2,16 +2,58 @@ $(document).ready(function(){
 
 	$('.bat-dart').click(function(){
 
-		$('.bat-hero-wrap').toggleClass('visible');
-		$('.bat-description-wrap').toggleClass('visible');
+		TweenMax.to(".landing-hero" , 2 , {
+			
+			position: "absolute",
+			ease: Back.easeOut.config(1),
+			bottom: "100%"
+
+		});
+
+		TweenMax.to(".bat-hero-wrap" , .8 , {
+
+			top: "0%",
+			ease: Back.easeOut.config(1),
+			display: "block"
+		});
+
+		TweenMax.to(".joker-hero-wrap" , 2 , {
+
+			top: "186%",
+			ease: Back.easeOut.config(1),
+			display: 'none'
+		});
 
 		
 	});
 
-	$('.joker-logo').click(function(){
-		$('.joker-hero-wrap').toggleClass('visible');
-		$('.joker-alt-description-wrap').toggleClass('visible');
-	});
 
+	$('.joker-logo').click(function(){
+
+		TweenMax.to(".joker-hero-wrap" , 2 , {
+
+			top: "0%",
+			display: 'block',
+			ease: Back.easeOut.config(1)
+		});
+
+		TweenMax.to(".landing-hero" , 2 , {
+			
+			position: "absolute",
+			bottom: "100%",
+			ease: Back.easeOut.config(1)
+
+		});
+
+		TweenMax.to(".bat-hero-wrap" , .8 , {
+
+			top: "100%",
+			display: "none",
+			ease: Back.easeOut.config(1)
+		});
+
+		TweenMax.to(".bat-dart")
+	});
 });
 
+	
