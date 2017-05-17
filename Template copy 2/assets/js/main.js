7325fcb294108a43e06a3fc1f10d84e1f1f2a9b9
babadav <babadav@gmail.com>
2017-05-16 23:44:26 -0400

    commit

--- Template copy 2/assets/js/main.js
+++ Template copy 2/assets/js/main.js
@@ -2,5 +2,23 @@ $(function(){
 
 	$('.menu-button').on('click', function(){
 		$('nav').toggleClass("open");
-	})
+	});
+
+	setTimeout(function(){
+		$('header').addClass('loaded');
+	}, 1000);
+
+	$('footer').on('click', function(){
+		$.scrollTo($('header'), 400);
+	});
+
+	$('.ad-container').parallax({imageSrc: 'http://lorempixel.com/output/food-q-c-1280-720-8.jpg'})
+
+
+	$('.text-fade-in').waypoint(function(){
+		$('.text-fade-in').addClass('show');
+	} , {
+		offset: '50%'
+	});
+
 });
